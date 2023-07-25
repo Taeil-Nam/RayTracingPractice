c10fa60857b8eb24857b3aabd43b25f85d302094
Taeil-Nam <tnam@student.42seoul.kr>
2023-07-25 12:40:36 +0900

    add: camera class 및 util 함수 추가

--- rtweekend.h
+++ rtweekend.h
@@ -20,6 +20,16 @@ inline double degrees_to_radians(double degrees) { // degree -> radian 변환
     return degrees * pi / 180.0;
 }
 
+inline double random_double() { // 실수 생성 함수
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
+
 // Common Headers
 
 #include "ray.h"
